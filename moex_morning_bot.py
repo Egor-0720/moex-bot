86b0762eb81355ad396e05d48940b3a2819c77b6
Egor-0720 <egorrostoff@mail.ru>
2026-04-18 14:20:59 +0300

    Add files via upload

--- moex_morning_bot.py
+++ moex_morning_bot.py
@@ -1,15 +1,15 @@
+print("Bot is starting...")  # Проверка, выводим сообщение, что бот начал запуск
+import time
+time.sleep(5)  # Задержка в 5 секунд, чтобы мы могли увидеть сообщение в логах
 import requests
-import os
-from dotenv import load_dotenv
+import time
 
-load_dotenv()  # Загружаем переменные из .env файла
-
-TOKEN = os.getenv('TELEGRAM_TOKEN')  # Токен берем из .env
-CHAT_ID = 173362390  # ID чата
+TOKEN = "8386523259:AAFyI25gMsunMf4VLC-EJ-jT4nE1ITu6hec"
+CHAT_ID = 173362390
 
 URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 def send(msg):
     requests.post(URL, data={"chat_id": CHAT_ID, "text": msg})
 
-send("🚀 Бот запущен")  # Отправляем сообщение о запуске бота
+send("🚀 Бот запущен")
