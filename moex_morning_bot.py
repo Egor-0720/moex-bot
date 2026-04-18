0c1076bb52e71c449461c48e507bdc74290e1bd5
Egor-0720 <egorrostoff@mail.ru>
2026-04-18 13:49:38 +0300

    Add files via upload

--- moex_morning_bot.py
+++ moex_morning_bot.py
@@ -1,10 +1,15 @@
+print("Bot is starting...")  # Проверка, выводим сообщение, что бот начал запуск
+import time
+time.sleep(5)  # Задержка в 5 секунд, чтобы мы могли увидеть сообщение в логах
 import requests
+import time
 
-TOKEN = "твой_токен"
+TOKEN = "8386523259:AAGs9w25ARfz0TazuC42kRSjnkR1R8CKA_U"
 CHAT_ID = 173362390
 
+URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
 def send(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+    requests.post(URL, data={"chat_id": CHAT_ID, "text": msg})
 
 send("🚀 Бот запущен")
